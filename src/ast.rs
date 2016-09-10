c0fe09ab5879dd0d64616910513d50c38b6e30bc
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-09-10 19:08:01 -0300

    tests: adding tests for tree eval

--- src/ast.rs
+++ src/ast.rs
@@ -29,19 +29,19 @@ impl Node {
     }
 }
 
-pub fn visit(node: Node) -> String {
+pub fn eval_tree(node: Node) -> String {
     let token = node.clone().token;
     match node.nodes() {
         (Some(lnode), Some(rnode)) => {
-            let lresult = visit(lnode);
-            let rresult = visit(rnode);
+            let lresult = eval_tree(lnode);
+            let rresult = eval_tree(rnode);
             return binary_operation(
                 lresult,
                 token.value,
                 rresult).to_string()
         },
-        (Some(lnode), None) => visit(lnode),
-        (None, Some(rnode)) => visit(rnode),
+        (Some(lnode), None) => eval_tree(lnode),
+        (None, Some(rnode)) => eval_tree(rnode),
         (None, None) => token.value
     }
 }
@@ -59,9 +59,33 @@ fn binary_operation(operand: String, operator: String, operand2: String) -> i32 
 }
 
 #[test]
-fn it_visits_leaf() {
+fn it_eval_tree_leaf() {
     let token = Token::build(Kind::Integer, String::from("10"));
     let leaf = Node::leaf(token);
 
-    assert_eq!("10", visit(leaf))
+    assert_eq!("10", eval_tree(leaf))
+}
+
+#[test]
+fn it_eval_the_node_binary_operation() {
+    let left = Node::leaf(Token::build(Kind::Integer, String::from("3")));
+    let operator = Token::build(Kind::Operator, String::from("+"));
+    let right = Node::leaf(Token::build(Kind::Integer, String::from("5")));
+    let node = Node::new(Some(left), operator, Some(right));
+
+    assert_eq!("8", eval_tree(node))
+}
+
+#[test]
+fn it_eval_complex_tree() {
+    let left = Node::leaf(Token::build(Kind::Integer, String::from("3")));
+    let operator = Token::build(Kind::Operator, String::from("*"));
+    let right = Node::leaf(Token::build(Kind::Integer, String::from("5")));
+    let plusnode = Node::new(Some(left), operator, Some(right));
+
+    let operator = Token::build(Kind::Operator, String::from("+"));
+    let sumright = Node::leaf(Token::build(Kind::Integer, String::from("5")));
+    let sumnode = Node::new(Some(plusnode), operator, Some(sumright));
+
+    assert_eq!("20", eval_tree(sumnode))
 }
