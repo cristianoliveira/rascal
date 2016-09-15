0074120ab61d013590d1c4376b66aab249714127
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-09-15 20:32:35 -0300

    feature: distribution as a library

--- tests/block_test.rs
+++ tests/block_test.rs
@@ -4,32 +4,32 @@ mod blocks {
 
     #[test]
     fn it_eval_retrieve_return_last_statement() {
-        let text =
+        let source =
         "begin
            mut x = 10;
            mut y = x + 5;
            return y + 5
          end";
 
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
         assert_eq!("20", result);
     }
 
     #[test]
     fn it_eval_bolean_expressions() {
-        let text =
+        let source =
         "begin
            imut x = 1;
            imut y = 2;
            return y == x
          end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
         assert_eq!("false", result);
     }
 
     #[test]
     fn it_eval_while_blocks() {
-        let text =
+        let source =
         "begin
            mut y = 0;
            while y < 4 begin
@@ -37,13 +37,13 @@ mod blocks {
            end;
            return y == 4
         end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
         assert_eq!("true", result);
     }
 
     #[test]
     fn it_eval_if_blocks() {
-        let text =
+        let source =
         "begin
            mut y = 0;
            if y < 4 begin
@@ -51,13 +51,13 @@ mod blocks {
            end;
            return y == 4
          end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
         assert_eq!("true", result);
     }
 
     #[test]
     fn it_eval_if_else_blocks() {
-        let text =
+        let source =
         "begin
            mut y = 0;
            if y > 4 begin
@@ -67,43 +67,43 @@ mod blocks {
            end;
            return y == 10
          end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
         assert_eq!("true", result);
     }
 
     #[test]
     #[should_panic]
     fn it_validate_immutable() {
-        let text =
+        let source =
         "begin
            imut y = 0;
            y = 1;
            return y
          end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
     }
 
     #[test]
     fn it_accepts_mutable() {
-        let text =
+        let source =
         "begin
            mut y = 0;
            y = 1;
            return y
         end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
         assert_eq!("1", result);
     }
 
     #[test]
     #[should_panic]
     fn it_validate_not_declared_var() {
-        let text =
+        let source =
         "begin
            imut y = 0;
            x = 1;
            return x
          end";
-        let result = rascal::eval(String::from(text));
+        let result = rascal::eval(String::from(source));
     }
 }
